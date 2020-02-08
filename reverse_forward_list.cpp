c5098425f198a902f8e5b47bcbedcdcb8187040a
nick <klepikov.nick@yandex.ru>
2020-02-08 19:19:05 +0300

    fix reverse_forward_list.cpp

--- reverse_forward_list.cpp
+++ reverse_forward_list.cpp
@@ -2,61 +2,71 @@
 using namespace std;
 
 struct node {
-	int data;
-	node* next;
+    int data;
+    node* next;
 
-	node():data{},next{nullptr} {}
-	node(int d, node* ptr):data{d},next{ptr} {}
+    node():data{},next{nullptr} {}
+    node(int d, node* ptr):data{d},next{ptr} {}
 };
 
 struct list{
-	node* head;
+    node* head;
 
-	list():head{nullptr} {}
+    list():head{nullptr} {}
 
-	void print() {
-		node* curr = head;
-		while(curr) {
-			cout << curr->data << ' ';
-			curr = curr->next;
-		}
-	}
+    ~list(){
+        while(head) {
+            node* temp = head;
+            head = head->next;
+            delete temp;
+        }
+    }
 
-        void reverse() {
-		node* curr = head->next;
-		node* prev = head;
-		prev->next=nullptr;
-		while(curr) {
-			head = curr;
-			curr = curr->next;
-			head->next = prev;
-			prev = head;
-		}
-	}
+    void print() {
+        node* curr = head;
+        while(curr) {
+            cout << curr->data << ' ';
+            curr = curr->next;
+        }
+    }
 
-	void push(int i) {
-		node* temp = new node(i, head);
-                head = temp;  
-	}
+    void reverse() {
+        if(!head)return;
+        node* curr = head->next;
+        node* prev = head;
+        prev->next=nullptr;
+        while(curr) {
+            head = curr;
+            curr = curr->next;
+            head->next = prev;
+            prev = head;
+        }
+    }
 
-	int pop() {
-		int res =0;
-		if(head) {
-		    res = head->data;
-		    node* temp = head;
-		    head = head->next;
-		    delete temp;
-		}
-		return res;
-	}
+    void push(int i) {
+        node* temp = new node(i, head);
+        head = temp;
+    }
+
+    int pop() {
+        int res =0;
+        if(head) {
+            res = head->data;
+            node* temp = head;
+            head = head->next;
+            delete temp;
+        }
+        return res;
+    }
 };
 
 int main() {
-	list l;
+    list l;
+    l.reverse();
         for(int i=10;i>0;--i) {
                 l.push(i);
-	}
-	l.print();cout << endl;
-	l.reverse();
-	l.print();cout << endl;
+    }
+    l.print();cout << endl;
+    l.reverse();
+    l.print();cout << endl;
 }
